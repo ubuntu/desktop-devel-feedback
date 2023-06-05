2a2be72c805bdc4d85907d8c9b345a78effdf25c
Tim Holmes-Mitra <tim@holmie.xyz>
2023-06-05 14:34:28 +0100

    feat(devscript): refactor dev serie enablement script
    - Colour code info and warn logging
    - Ask for user confirm before proceeding
    - Refactor configs into functions for clearer intention

--- scripts/enable-desktop-experimental.sh
+++ scripts/enable-desktop-experimental.sh
@@ -1,45 +1,130 @@
 #!/bin/bash
 
+#
+# Preamble
+#
+
 set -e
 # use english to be able to easily grep from expected strings
 export LC_ALL=C.UTF-8
 unset LANG LANGUAGE
-
-cmd() { echo "+" "$@" ; "$@" ; }
-
 devserie="mantic"
 
-if ! grep -q $devserie /etc/apt/sources.list; then
-  echo "you need to be on $devserie to use this script, update your apt sources definition"
-  exit 1
-fi
+#
+# Helpers
+#
 
-if ! grep -qr "$devserie-proposed" /etc/apt; then
-  echo "* Enabling $devserie-proposed sources"
-  cmd sudo add-apt-repository -S "deb http://archive.ubuntu.com/ubuntu/ $devserie-proposed universe main restricted multiverse"
+GREEN="\033[1;32m"
+ORANGE="\033[0;33m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
+info() {
+  echo -e "${GREEN}INFO: $@${RESET} "
+}
+
+warn() {
+  echo -e "${ORANGE}WARN: $@${RESET} "
+}
+
+error() {
+  echo -e "${RED}ERROR: $@${RESET}" 1>&2
+}
+
+newline() {
   echo ""
-fi
+}
 
-echo "* Installing the ubuntu shell extensions including tiling"
-cmd sudo apt install gnome-shell-ubuntu-extensions
-echo ""
+#
+# Functions
+#
 
-echo "* Installing the new polkit daemon, it might make some of your rules invalid"
-cmd sudo apt install libpolkit-agent-1-0/$devserie-proposed libpolkit-gobject-1-0/$devserie-proposed pkexec/$devserie-proposed policykit-1/$devserie-proposed polkitd/$devserie-proposed gir1.2-polkit-1.0/$devserie-proposed
-echo ""
+cmd() {
+  newline
+  info "$@" # Log the command to be run
+  "$@"      # Run the command
+  newline
+}
 
-echo "* Installing dbus-broker (not removing dbus-daemon though since gdm currently depends on it)"
-cmd sudo apt install dbus-broker #dbus-daemon-
-echo ""
+welcome_message() {
+  newline
+  info "Thank you for testing Mantic Minotaur 🐂🏃"
+  info "This script configures your machine to be as close to the final version as possible."
+  newline
+  warn "This is under active development so expect breakages."
+  newline
+}
 
-echo "* Installing the new flutter snap-store from preview/edge"
-if snap list snap-store &>/dev/null; then
-  cmd sudo snap refresh --channel=preview/edge snap-store
-else
-  cmd sudo snap install --channel=preview/edge snap-store
-fi
-echo ""
+check_environment() {
+  if ! grep -q $devserie /etc/apt/sources.list; then
+    error "You need to be on $devserie to use this script"
+    exit 1
+  fi
+}
 
-echo "* Installing the new flutter firmware-updater snap from edge"
-cmd sudo snap install --edge firmware-updater
-echo ""
+ask_if_user_wants_to_continue() {
+  info "Mantic Minotaur detected 🥳"
+  read -p "Y or y to continue: " -n 1 -r
+  newline
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+}
+
+update() {
+  info "Updating OS ..."
+  cmd sudo apt update -y && sudo apt upgrade && sudo snap refresh
+}
+
+configure_sources() {
+  if ! grep -qr "$devserie-proposed" /etc/apt; then
+    info "Enabling $devserie-proposed sources ..."
+    cmd sudo add-apt-repository -S "deb http://archive.ubuntu.com/ubuntu/ $devserie-proposed universe main restricted multiverse"
+  fi
+}
+
+install_gnome_shell_extensions() {
+  info "Installing the ubuntu shell extensions including tiling"
+  cmd sudo apt install -y gnome-shell-ubuntu-extensions
+}
+
+install_polkit() {
+  info "Installing the new polkit daemon"
+  warn "This might make old rules invalid"
+  cmd sudo apt install -y libpolkit-agent-1-0/$devserie-proposed libpolkit-gobject-1-0/$devserie-proposed pkexec/$devserie-proposed policykit-1/$devserie-proposed polkitd/$devserie-proposed gir1.2-polkit-1.0/$devserie-proposed
+}
+
+install_dbus_broker() {
+  info "Installing dbus-broker (not removing dbus-daemon though since gdm currently depends on it)"
+  cmd sudo apt install -y dbus-broker #dbus-daemon-
+}
+
+install_app_store() {
+  if snap list snap-store &>/dev/null; then
+    info "Refreshing the app store from preview/edge"
+    cmd sudo snap refresh --channel=preview/edge snap-store
+  else
+    info "Installing the app store from preview/edge"
+    cmd sudo snap install --channel=preview/edge snap-store
+  fi
+}
+
+install_firmware_updater() {
+  info "Installing the new flutter firmware-updater snap from edge"
+  cmd sudo snap install --edge firmware-updater
+}
+
+main() {
+  welcome_message
+  check_environment
+  ask_if_user_wants_to_continue
+  update
+  configure_sources
+  install_gnome_shell_extensions
+  install_polkit
+  install_dbus_broker
+  install_app_store
+  install_firmware_updater
+}
+
+main
